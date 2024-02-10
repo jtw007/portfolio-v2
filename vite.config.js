ac85cd672cad43d29a30cd54f602534f5f77b1a4
jtw007 <joshwu095@gmail.com>
2024-02-09 16:35:48 -0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // sets default port to localhost:3000 
+    port: 3000,
+    proxy: {
+      '/api-v1': 'http://localhost:8000/',
+    }
+  }
+})
