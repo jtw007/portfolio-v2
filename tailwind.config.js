abe0d7db86aa1d928c4df7f7324cb696ff26409f
jtw007 <joshwu095@gmail.com>
2024-02-21 14:40:26 -0800

    tailwind setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+      screens: {
+        // @media (min-width: 375px) smallest screen, tablet size 834px, web screen 1920px
+        'xs': '320px',
+        'sm': '375px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1920px',
+      }
+    },
+    plugins: [],
+  }
