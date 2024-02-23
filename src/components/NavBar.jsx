344c172ab43cecad85a1803a4d0676dbc29e457c
jtw007 <joshwu095@gmail.com>
2024-02-22 17:08:45 -0800

    navbar setup finished, layout next

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    
+    return (
+        <div className=''>
+            <a href='/'>
+                <div className=''>
+                    <h1>joshwu.dev</h1>
+                </div>
+            </a>
+
+            <ul className=''>
+                <li className=''>one</li>
+                <li className=''>two</li>
+                <li className=''>three</li>
+            </ul>
+        </div>
+    )
 }
 
 export default NavBar
