6ed34df0c94b18abbc8ce2f16e66619f414540f6
jtw007 <joshwu095@gmail.com>
2024-02-27 18:16:54 -0800

    some edits made to about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,12 @@
 
 const About = () => {
     return (
-        <div className="flex justify-center flex-col mt-5">
-            <h1 className="ml-5 justify-center font-bold text-3xl">About Me</h1>
+        <div className="flex xs:items-center w-full flex-col my-16 border border-green-500">
+            <h1 className="ml-7 my-5 flex self-start font-bold text-3xl border border-blue-300">About Me</h1>
 
-            <div className="flex lg:flex-row xs:flex-col items-center justify-around border border-red-500">
+            <div className="flex lg:flex-row xs:flex-col items-center justify-around mx-7 xs:max-w-[330px] sm:max-w-[375px] md:max-w-[1000px] lg:max-w-[1200px] xl:max-w-[1500px] border border-red-500">
                 <div className="">
-                    <img className="rounded-full relative" src='../../../photo-assets/profilePicture1.png' alt='photo of me'/>
+                    <img className="rounded-full relative p-5 xs:width-55vw md:max-w-[400px]" src='../../../photo-assets/profilePicture1.png' alt='photo of me'/>
                 </div>
 
                 <div className="">
