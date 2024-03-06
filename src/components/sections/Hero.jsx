6cbd4abcbdb1b28f12f92183eb8ed3c32760c9e1
jtw007 <joshwu095@gmail.com>
2024-03-06 18:14:31 +0800

    created and began on hero section

---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,22 @@
+
+const Hero = () => {
+    return (
+        <div className="">
+
+            <div className="">
+                <h1 className="">Josh Wu</h1>
+            </div>
+
+            <div className="">
+                <h1 className="">Frontend Developer</h1>
+            </div>
+
+            <div className="">
+                <h1 className="">Let's build something together!</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,8 +1,10 @@
 import About from "./About";
+import Hero from "./Hero";
 
 const Home = () => {
     return (
         <div className='body'>
+            <Hero />
             <About /> 
         </div>
     )
