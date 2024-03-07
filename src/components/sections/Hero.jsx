436ab9bea2238c7524f330b468a22b28fa68ac2c
jtw007 <joshwu095@gmail.com>
2024-03-07 16:18:32 +0800

    some styling and rough styles added to hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,18 +1,18 @@
 
 const Hero = () => {
     return (
-        <div className="">
+        <div className="p-20 flex flex-col items-center bg-red-500">
 
-            <div className="">
-                <h1 className="">Josh Wu</h1>
+            <div className="py-10">
+                <h1 className="text-6xl">Josh Wu</h1>
             </div>
 
-            <div className="">
-                <h1 className="">Frontend Developer</h1>
+            <div className="py-2">
+                <h1 className="text-2xl">Frontend Developer</h1>
             </div>
 
-            <div className="">
-                <h1 className="">Let's build something together!</h1>
+            <div className="py-2">
+                <h1 className="text-2xl">Let's build something together!</h1>
             </div>
 
         </div>
