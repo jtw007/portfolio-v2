ce937d17ba644682ae3ea9e4c342652b15006373
jtw007 <joshwu095@gmail.com>
2024-03-13 12:35:32 +0800

    added formatting for md and lg screens

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -9,17 +9,17 @@ const Projects = () => {
                 <h1 className="font-bold text-3xl">JoshWu.Projects</h1>
             </div>
 
-            <div className='w-full xs:max-w-[330px] sm:max-w-[375px] md:max-w-[1000px] lg:max-w-[1200px] xl:max-w-[1500px] self-center flex lg:flex-row xs:flex-col xs:items-center'>
+            <div className='w-full xs:max-w-[330px] sm:max-w-[375px] md:max-w-[1000px] lg:max-w-[1200px] xl:max-w-[1500px] self-center flex lg:flex-row lg:justify-center md:flex-row md:justify-center md:px-2  xs:flex-col xs:items-center'>
                 {projectsList.map((project, idx) => {
                     return (
                         <Link key={`projects-${idx}`} to={`/${project.slug}`}> 
-                            <div className='flex flex-row justify-center items-center bg-no-repeat bg-center bg-cover lg:h-[300px] lg:w-[500px] xs:h-[200px] xs:w-[320px] xs:my-2' style={{backgroundImage:`url(${project.img})`}}
+                            <div className='flex flex-row justify-center items-center bg-no-repeat bg-center bg-cover xl:h-[300px] xl:w-[500px] xl:mx-5 lg:mx-5 md:h-[200px] md:w-[350px] md:my-3 md:mx-2 xs:h-[200px] xs:w-[320px] xs:my-2' style={{backgroundImage:`url(${project.img})`}}
                             >
-                                <span className='d:none d-lg-inline'>
+                                <span className='lg:hidden xl:inline'>
                                     {project.name}
                                 </span>
                             </div>
-                            <div className='md:hidden xs:flex xs:justify-center'>
+                            <div className='xl:hidden xs:flex xs:justify-center'>
                                 {project.name}
                             </div>
                         </Link>
