47b65f5f50ae3ad4936f8eab40ed7862236f3c23
jtw007 <joshwu095@gmail.com>
2024-03-14 09:59:09 +0800

    began work on project details page

--- src/components/sections/ProjectDetails.jsx
+++ src/components/sections/ProjectDetails.jsx
@@ -2,9 +2,28 @@ import { projectsList } from "../../portfolio-data/projectsList"
 import { useParams } from "react-router-dom"
 
 const ProjectDetails = () => {
+    const { slug } = useParams()
+    const project = projectsList.find((project) => project.slug === slug)
+
+    function openSite() {
+        window.open(project.siteURL)
+    }
+
+    function openGithub() {
+        window.open(project.githubURL)
+    }
+
     return (
         <div className="">
-
+            <header className="">
+                <div>
+                    <img 
+                        className=""
+                        src={project.img}
+                        alt='project image'
+                    />
+                </div>
+            </header>
         </div>
     )
 }
