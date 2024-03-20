5cddbda10b9f789cf435cf522f21335019b0293c
jtw007 <joshwu095@gmail.com>
2024-03-20 12:04:11 +0800

    some more styling/formatting added to projectdetails

--- src/components/sections/ProjectDetails.jsx
+++ src/components/sections/ProjectDetails.jsx
@@ -16,54 +16,59 @@ const ProjectDetails = () => {
     return (
         <div className="my-5 py-16">
             {/* header start */}
-            <header className="py-[10px] px-[14px] flex justify-center border border-red-500">
-                <div className="max-w-[72rem] flex flex-row justify-between border border-blue-500">
+            <header className="py-[10px] px-[14px] flex  justify-center border border-red-500">
+                <div className="flex justify-between px-5 border-2 border-blue-500">
                     <img 
                         className="max-w-[500px]"
                         src={project.img}
                         alt='project image'
                     />
+                    <div className="flex flex-col px-5 border-2 border-green-700">
+                        
+                        <div className="flex flex-col justify-start items-center gap-10">
+                            <p className="bold text-4xl text-center py-5">{project.name}</p>
+                        </div>
 
-                    <div className="flex flex-col justify-start items-center gap-10">
-                        <p className="bold text-4xl text-center">{project.name}</p>
-                    </div>
+                        <div className="flex flex-col gap-5 items-center">
+                            <p className="block text-center py-3">{project.subheader}</p>
+                        </div>
+                        {/* buttons */}
+                        <div className="flex flex-row gap-3 my-2">
+                            <button 
+                                className="border rounded-[15px] leading-4 font-bold uppercase py-2 px-5"
+                                onClick={() => openSite()}
+                            >
+                                Deployed Site
+                            </button>
 
-                    <div className="flex flex-col gap-5 items-center">
-                        <p className="block text-center">{project.subheader}</p>
-                    </div>
-                    {/* buttons */}
-                    <div className="flex flex-row gap-3 ">
-                        <button 
-                            className="border rounded-[15px] leading-4 font-bold uppercase py-2 px-5"
-                            onClick={() => openSite()}
-                        >
-                            Deployed Site
-                        </button>
-
-                        <button 
-                            className="border rounded-[15px] leading-4 font-bold uppercase py-2 px-5"
-                            onClick={() => openGithub()}
-                        >
-                            Github Repo
-                        </button>
+                            <button 
+                                className="border rounded-[15px] leading-4 font-bold uppercase py-2 px-5"
+                                onClick={() => openGithub()}
+                            >
+                                Github Repo
+                            </button>
+                        </div>
                     </div>
                     
-
+                    
                 </div>
             </header>
             {/* header end */}
-            <div className="max-w-[56rem] flex flex-col items-center gap-9">
+            <div className="max-w-[56rem] flex flex-col items-center gap-9 border border-yellow-600">
                 <p className="text-2xl font-bold">
                     Built with
                 </p>
-                <ul className="">
-                    <li className="" key={`project`}>
-                        <span className="">
+                <ul className="gap-7 flex justify-center flex-wrap">
+                    <li 
+                        className="flex flex-col justify-center items-center gap-2" 
+                        key={`project`}
+                    >
+                        <span className="font-bold text-xl">
                             {project.stacks.toString().split(',').join(', ')}
                         </span>
                     </li>
                 </ul>
-                <p className="">
+                <p className="text-center px-8">
                     {project.desc}
                 </p>
             </div>
