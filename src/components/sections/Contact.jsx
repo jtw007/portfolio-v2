0a504004c0a478e7900037a536708cbece2e14d9
jtw007 <joshwu095@gmail.com>
2024-04-09 14:05:46 +0800

    some work added to contact, removed unnecessary div in projects

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className="my-5 py-5" id='contact'>
+            <div className="">
+                <div className="flex justify-center">
+                    <h1 className="font-bold text-3xl">Connect with me!</h1>
+                </div>
+                <div className="flex justify-center">
+                    <button>LinkedIn</button>
+                    <button>Github</button>
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -12,18 +12,15 @@ const Projects = () => {
                 <h1 className="font-bold text-3xl">JoshWu.Projects</h1>
             </div>
 
-            <div className='w-full xs:max-w-[330px] sm:max-w-[375px] md:max-w-[1000px] lg:max-w-[1200px] xl:max-w-[1500px] self-center flex lg:flex-row lg:justify-center md:flex-row md:justify-center md:px-2 xs:flex-col xs:items-center xs:py-5'>
+            <div className='w-full xs:max-w-[330px] sm:max-w-[375px] md:max-w-[1000px] lg:max-w-[1200px] xl:max-w-[1500px] self-center flex lg:flex-row lg:justify-center md:flex-row md:justify-center md:px-2 xs:flex-col xs:items-center xs:py-5 border border-blue-400'>
                 {projectsList.map((project, idx) => {
                     return (
-                        <Link key={`projects-${idx}`} to={`/${project.slug}`} className=' group'> 
-                            <div className='flex flex-row justify-center items-center bg-no-repeat bg-center bg-cover xl:h-[300px] xl:w-[500px] xl:mx-5 lg:mx-5 md:h-[200px] md:w-[350px] md:my-3 md:mx-2 xs:h-[200px] xs:w-[320px] xs:my-2 xl:group-hover:opacity-75' style={{backgroundImage:`url(${project.img})`}}
-                            >
+                        <Link key={`projects-${idx}`} to={`/${project.slug}`} className='group'> 
+                            <div className='flex flex-row justify-center items-center bg-no-repeat bg-center bg-cover xl:h-[300px] xl:w-[500px] xl:mx-10 lg:px-5 md:h-[200px] md:w-[350px] md:my-3 md:mx-2 xs:h-[200px] xs:w-[320px] xs:my-2 xl:group-hover:opacity-75 border border-red-500' style={{backgroundImage:`url(${project.img})`}}
+                            >   
                                 <span className='lg:hidden xs:hidden xl:group-hover:inline xl:text-xl'>
                                     {project.name}
                                 </span>
-                            </div>
-                            <div className='xl:hidden xs:flex xs:justify-center'>
-                                {project.name}
                             </div>
                         </Link>
                     )
