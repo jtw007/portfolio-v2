02afe3b07f8aec8f9c6fda702a9b1e1c8dde7a01
jtw007 <joshwu095@gmail.com>
2024-04-20 14:07:00 +0800

    added hover animations to contact icons

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -13,14 +13,14 @@ const Contact = () => {
                         <img 
                             src={githubIcon} 
                             role='button'
-                            className='h-[50px] w-[50px] mx-5'
+                            className='h-[50px] w-[50px] mx-5 lg:hover:scale-125 lg:duration-500'
                         />
                     </a>
                     <a href='https://www.linkedin.com/in/joshwudev/' target='_blank' rel='noopener noreferrer'>
                         <img 
                             src={linkedInIcon} 
                             role='button'
-                            className='h-[50px] w-[50px] mx-5'
+                            className='h-[50px] w-[50px] mx-5 lg:hover:scale-125 lg:duration-500'
                         />
                     </a>
                         
